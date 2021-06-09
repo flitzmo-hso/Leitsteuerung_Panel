93703baa88dbc7b96bd033d9577bcc372ce155cf
sauerma <msauer1@hs-offenburg.de>
2021-06-09 13:41:08 -0700

    Blanc

---
+++ src/components/Dashboard.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import ManualOrder from './ManualOrder.js';
+
+
+//Set Style
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+  },
+  container: {
+  },
+  paper: {
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
+
+export default function Dashboard() {
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+
+          <Grid container spacing={2}>
+
+            <Grid item xs={12} md={6} lg={9}>
+              <Paper className={fixedHeightPaper}>
+                <ManualOrder />
+              </Paper>
+            </Grid>
+
+            <Grid item xs={12} md={6} lg={3}>
+              <Paper className={classes.paper} style={{  padding: 0, marginBottom: 3, flexDirection: "revert"}}>
+               <h2>&nbsp; Auslastung</h2>
+              </Paper>
+              <Paper className={classes.paper} style={{  padding: 0, marginBottom: 3, flexDirection: "revert" }}>
+               <h2>&nbsp; Fortschritt</h2>
+              </Paper>
+              <Paper className={classes.paper} style={{  padding: 0, margin: 1, flexDirection: "revert" }}>
+               <h2>&nbsp; Privatkunden</h2>
+              </Paper>
+            </Grid>  
+
+            <Grid item xs={12} md={12}>
+              <Paper className={classes.paper}>
+           
+              </Paper>
+            </Grid>
+
+          </Grid>
+
+        </Container>
+      </main>
+    </div>
+  );
+}
---
+++ src/components/ManualOrder.js
@@ -0,0 +1,139 @@
+import React, {useState} from "react";
+import Button from '@material-ui/core/Button';
+import axios from "axios";
+
+export default function ManualTask() {
+
+ const [DeliveryVisibility, setDeliveryVisibility] = useState("inline");
+ const [LoopVisibility, setLoopVisibility] = useState("none");
+
+   /*[                  ---------------EXAMPLE----------------
+      {"task_type":"Loop", "start_time":0, "priority":0, "description": {"num_loops":5, "start_name":"coe", "finish_name":"lounge"}},
+      {"task_type":"Delivery", "start_time":0, "priority":0, "description": {"option": "coke"}},
+      {"task_type":"Loop", "start_time":0, "priority":0, "description": {"num_loops":5, "start_name":"pantry", "finish_name":"supplies"}}
+    ] */
+
+
+  function getFormValues(){
+
+    var startzeit = document.getElementById('startzeit').value;
+    var prio = document.getElementById('prio').value;
+    var auftragstyp = document.getElementById('auftragstyp').value; 
+    var auftragDelivery = document.getElementById('auftragDelivery').value; //Delivery
+    var startLoc = document.getElementById('startLoc').value; //Loop
+    var endLoc = document.getElementById('endLoc').value; //Loop
+    var numberLoops = document.getElementById('numberLoops').value; //Loop
+
+    var obj = {}
+
+    //Delivery Order
+    if (auftragstyp === "Delivery") {
+
+      if (startzeit === '' || prio === '' || auftragDelivery === '')
+         { return undefined; }
+
+      obj['task_type'] = "Delivery"; obj['start_time'] = startzeit; obj['priority'] = prio; 
+      obj['description'] = {"option": auftragDelivery};
+    }
+
+    //Loop Order
+    if (auftragstyp === "Loop") {
+
+      if(startzeit === '' || prio === '' || numberLoops === '' || 
+         numberLoops === 0 || startLoc === '' || endLoc === '')  { return undefined;} 
+  
+      obj['task_type'] = "Loop"; obj['start_time'] = startzeit; obj['priority'] = prio;
+      obj['description'] = {"num_loops": numberLoops , "start_name": startLoc, "finish_name": endLoc};
+   
+    }
+
+    return [obj];
+
+  } 
+
+
+  function sendToAgv(){
+
+    var objVal = getFormValues();
+    if(objVal === undefined || objVal.length === 0) {alert("Bitte richtige Daten eingeben."); return;} 
+    console.log("Auftrag:", objVal)
+
+    axios.post('http://0.0.0.0:8080/submit_task', objVal)
+    .then(res => {
+    console.log("RESPONSE:", res);
+    alert("Erfolgreich übermittelt."); 
+  
+    })
+    .catch(err => {
+        console.log(err.message); //Error-Handling
+        alert("Fehler.");  
+  
+    }) 
+
+    return;
+  }
+ 
+  //Visibility of input fields for specific order typ
+  function dropDownChange(newValue){
+
+    if(newValue === undefined || newValue === null) return;
+
+    //Loop
+    if (newValue === "Loop") {
+      setDeliveryVisibility("none");
+      setLoopVisibility("inline");
+    }
+    
+    //Delivery
+    if (newValue === "Delivery") {
+      setDeliveryVisibility("inline");
+      setLoopVisibility("none");
+    }
+  } 
+
+  return (
+    <div>
+ <p>Manueller Fahrauftrag</p>
+ <table align="center"> 
+      <tr>
+        <td colspan="1" >
+        <select name="auftragstyp"  id="auftragstyp" onChange={e => dropDownChange(e.target.value)}>
+            <option  value="Delivery"> Delivery</option>
+            <option  value="Loop" >Loop</option>
+           {/*<option  value="Clean" >Clean</option> */} 
+          </select>
+          
+          </td> 
+      </tr>
+      <tr >
+        <td colspan="1"> <input type="number" id="startzeit" /> Startzeit (Minuten von jetztab)</td>
+      </tr>
+      <tr >
+        <td colspan="1"> <input type="number" id="prio" /> Priorität </td>
+      </tr>
+      <tr id = "Delivery" >
+        <td colspan="1" style={{display: DeliveryVisibility}}> <input type="text" id="auftragDelivery" /> Auftrag-Delivery</td>
+      </tr>
+      <tr id = "Loop" >
+        <td colspan="1" style={{display: LoopVisibility}}> <input type="text" id="startLoc" /> Start-Location</td>
+      </tr>
+      <tr id = "Loop" >
+        <td colspan="1" style={{display: LoopVisibility}}> <input type="text" id="endLoc" /> End-Location</td>
+      </tr>
+      <tr id = "Loop" >
+        <td colspan="1" style={{display: LoopVisibility}}> <input type="number" id="numberLoops" />Nummer an Loops</td>
+      </tr>
+      <tr >
+      <Button 
+    //variant="contained" 
+    style={{backgroundColor: "gray"}}
+    onClick={sendToAgv} 
+    title="Mit Klick auf diesen Button werden manuelle Fahraufträge weitergegeben." >
+    Absenden
+  </Button>
+      </tr>
+  </table>
+    </div>
+    
+  );
+}
